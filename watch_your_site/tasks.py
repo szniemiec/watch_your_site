de8d5e86004028b531ff42c55985bc1f0209b511
Szymon Niemiec <szniemiec@yahoo.com>
2021-03-13 04:57:41 -0800

    Adding schedule works, still working on adding task

--- watch_your_site/tasks.py
+++ watch_your_site/tasks.py
@@ -9,17 +9,16 @@ from watch_your_site.celery import app
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sites = Site.objects.all()
-    for site in sites:
-        site_url = site.site_url
+    for site in Site.objects.all():
         check_interval = site.check_interval
         schedule = IntervalSchedule.objects.create(every=check_interval, period=IntervalSchedule.SECONDS)
-        sender.add_periodic_task(interval=schedule, task='tasks.fetch_url',
-                                 name='Task for ' + site_url + ' | Interval: ' + check_interval)
+        sender.add_periodic_task(schedule, fetch_url.s(site.site_url),
+                                 name=site.site_url)
 
 
 @app.task
-def fetch_url(site):
-    http_code = requests.get(site.site_url).status_code
+def fetch_url(url):
+    http_code = requests.get(url).status_code
+    site = Site.objects.get(url)
     celery_result = CeleryResult(http_code=http_code, date=datetime.datetime.now(), site=site.site_id)
     CeleryResult.save(celery_result)
