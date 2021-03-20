88f22232840b87a6d377481bd5863aeaebff9c80
Szymon Niemiec <szniemiec@yahoo.com>
2021-03-20 04:05:51 -0700

    Add display of fields in admin panel tables

--- database/admin.py
+++ database/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Task, Result
 
-admin.site.register([Task, Result])
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'url', 'interval']
+
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'http_code', 'date', 'task_id']
+
+
+admin.site.register(Task, TaskAdmin)
+
+admin.site.register(Result, ResultAdmin)
