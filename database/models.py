34e9e06ec55a6d8c4cb99c99d4f9310b2d194e0f
Szymon Niemiec <szniemiec@yahoo.com>
2021-03-20 05:12:13 -0700

    Remove unnecessary __str__ method

--- database/models.py
+++ database/models.py
@@ -8,17 +8,9 @@ class Task(models.Model):
     url = models.TextField(max_length=200, default='url error')
     interval = models.PositiveIntegerField(default=15)
 
-    def __str__(self):
-        str = self.id + ',' + self.url + ',' + self.interval
-        return str
-
 
 class Result(models.Model):
     id = models.AutoField(primary_key=True)
     http_code = models.TextField(max_length=200, default='method error')
     date = models.DateTimeField(default=now())
     task_id = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        str = self.id + ',' + self.http_code + ',' + self.date + ',' + self.task_id
-        return str
